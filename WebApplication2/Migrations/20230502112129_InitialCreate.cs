9ccd34ae3dfdda02bdf7d5494193d98665d815fd
Igor <igorshevelenkov@gmail.com>
2023-05-12 00:12:34 +0300

    Add project files.

---
+++ WebApplication2/Migrations/20230502112129_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "File",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "File");
+        }
+    }
+}
---
+++ WebApplication2/Models/File.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string? path { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateModified { get; set; }
+    }
+}
